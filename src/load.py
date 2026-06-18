5c31dd0b97f384463779d1190175f94b2d4c798d
José Donado <josepablo.donado@gmail.com>
2026-06-18 10:53:45 -0600

    feat: integrate the ETL with main.py.

---
+++ src/load.py
@@ -0,0 +1,57 @@
+import json
+import logging
+
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+
+def load_weather_data():
+
+    with open(
+        "data/processed/weather_processed.json",
+        "r"
+    ) as file:
+        data = json.load(file)
+
+    engine = create_engine(
+        "postgresql+psycopg2://weather_user:weather_password@localhost:5432/weather_db"
+    )
+
+    query = text("""
+        INSERT INTO weather_observations (
+            observation_time,
+            temperature_celsius,
+            humidity_percent,
+            wind_speed_kmh
+        )
+        VALUES (
+            :observation_time,
+            :temperature_celsius,
+            :humidity_percent,
+            :wind_speed_kmh
+        )
+    """)
+
+    with engine.connect() as connection:
+
+        connection.execute(
+            query,
+            data
+        )
+
+        connection.commit()
+
+    logging.info(
+        "Weather observation inserted successfully."
+    )
+
+    return data
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO)
+
+    result = load_weather_data()
+
+    print(result)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
 from extract import extract_weather_data
 from transform import transform_weather_data
+from load import load_weather_data
 
-# For Guatemala
 LATITUDE = 15.5
 LONGITUDE = -90.25
 
-raw_data = extract_weather_data(
-    latitude=LATITUDE,
-    longitude=LONGITUDE
-)
 
-if raw_data:
-    processed_data = transform_weather_data()
+def run_pipeline():
 
-    print("Pipeline executed successfully.")
+    extract_weather_data(
+        latitude=LATITUDE,
+        longitude=LONGITUDE
+    )
+
+    transform_weather_data()
+
+    load_weather_data()
+
+    print(
+        "Pipeline executed successfully."
+    )
+
+
+if __name__ == "__main__":
+    run_pipeline()
